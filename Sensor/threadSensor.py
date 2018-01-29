34f3a4381134138fe9d84ef9b7c229e15c4dc71d
crisos <armen.hovhannissian@gmail.com>
2018-01-29 18:18:30 +0100

    bla

---
+++ Sensor/threadSensor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding: utf-8	
+import DistanceSensor as ds
+import RPi.GPIO as GPIO
+def main():
+	dSens = ds.DistanceSensor(25,24,GPIO.BCM)
+	while(1)
+		print(dSens.mesuring)
+
+	dSens.cleanup()
+
+if __name__ =='__main__':
+	main()
