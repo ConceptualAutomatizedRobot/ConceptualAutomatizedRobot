b7176b36273f3013af8032ca1cd472fbbc153f4a
crisos <armen.hovhannissian@gmail.com>
2018-01-29 17:44:35 +0100

    blaazeaze

--- Sensor/testDistanceSensor.py
+++ Sensor/testDistanceSensor.py
@@ -3,11 +3,11 @@
 import DistanceSensor as ds
 import RPi.GPIO as GPIO
 def main():
-	print("salut")
 	dSens = ds.DistanceSensor(25,24,GPIO.BCM)
 	print(" Trig "+str(dSens.Trig))
-	print("Echo "+str(dSens.Echo))
-	print(dSens.getDistance())
+	print(" Echo "+str(dSens.Echo))
+	#print(dSens.getDistance())
+	dSens.mesuring()
 	dSens.cleanup()
 
 if __name__ =='__main__':
