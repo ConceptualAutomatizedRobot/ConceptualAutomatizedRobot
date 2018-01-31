3e86dfa961ad68580fe5b84e7a0a548af8058c01
crisos <armen.hovhannissian@gmail.com>
2018-01-31 19:18:49 +0100

    bla bla

--- Sensor/threadSensor.py
+++ Sensor/threadSensor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8	
+import sys
 import DistanceSensor as ds
 import RPi.GPIO as GPIO
 def main():
@@ -10,7 +11,7 @@ def main():
 	except KeyboardInterrupt:
 		print(" Exiting program")
 	except:
-		print(" Other exception detected")
+		print(" Other exception detected\n"+str(sys.exc_info()[0]) )
 	finally:
 		dSens.cleanup()
 
