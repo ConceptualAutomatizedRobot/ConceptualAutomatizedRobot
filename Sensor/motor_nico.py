1294c7ef393d633d2dddd7d66c8d1ea68b289d18
crisos <armen.hovhannissian@gmail.com>
2018-01-31 17:09:57 +0100

    bla bla

--- Sensor/motor_nico.py
+++ Sensor/motor_nico.py
@@ -11,6 +11,12 @@ import car_dir as cd
 import time
 
 class Motor:
+
+    Motor0_A = 11  # pin11
+    Motor0_B = 16  # pin16
+    Motor1_A = 13  # pin13
+    Motor1_B = 15  # pin15
+    pins = [Motor0_A, Motor0_B, Motor1_A, Motor1_B]
     def __init__(self):
         m.setup() #Have to be done
     ############################################
