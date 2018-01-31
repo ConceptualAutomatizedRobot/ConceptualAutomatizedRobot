9774e4b2edcbddbf425435d680953cb2ffb7b13d
crisos <armen.hovhannissian@gmail.com>
2018-01-31 19:16:49 +0100

    bla bla

--- Sensor/threadSensor.py
+++ Sensor/threadSensor.py
@@ -4,10 +4,15 @@ import DistanceSensor as ds
 import RPi.GPIO as GPIO
 def main():
 	dSens = ds.DistanceSensor(16,18,GPIO.BCM)
-	while(1):
-		print(dSens.mesuring())
-
-	dSens.cleanup()
+	try:
+		while(1):
+			print(dSens.mesuring())
+	except KeyboardInterrupt:
+		print(" Exiting program")
+	except:
+		print(" Other exception detected")
+	finally:
+		dSens.cleanup()
 
 if __name__ =='__main__':
 	main()
