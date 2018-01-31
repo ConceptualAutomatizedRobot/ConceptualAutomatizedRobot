0692e445d1acd3c78b241ef90c4813d0233b4bde
crisos <armen.hovhannissian@gmail.com>
2018-01-31 19:01:37 +0100

    bla bla

--- Sensor/testDistanceSensor.py
+++ Sensor/testDistanceSensor.py
@@ -3,8 +3,9 @@
 import DistanceSensor as ds
 import RPi.GPIO as GPIO
 def main():
-	GPIO.setmode(GPIO.BCM)
-	p=GPIO.PWM(16, 1)
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(12, GPIO.OUT)
+	p=GPIO.PWM(12, 1)
 	p.start(1)
 	"""dSens = ds.DistanceSensor(16,18,GPIO.BCM)
 	print(" Trig "+str(dSens.Trig))
