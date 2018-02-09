6ad289afb2ae5c3a2a4e1d956cc3be9055b59e1b
plume <fortfrederic@free.fr>
2018-02-09 11:31:25 +0100

    sensors/camera.py: added support for software max fps

--- sensors/camera.py
+++ sensors/camera.py
@@ -1,16 +1,20 @@
 import cv2 as cv
 import time
 import numpy as np
+import time
 
 class Camera:
     _cam = None
     _capture = None
+    _last = None
+    _fps = None
 
     def __init__(self, cam, fps=None, resolution=None):
         self._cam = cam
         self._capture = cv.VideoCapture(self._cam)
         if fps is not None:
             self._capture.set(cv.CAP_PROP_FPS, fps)
+            self._fps = fps
         if resolution is not None:
             w = self._capture.get(cv.CAP_PROP_FRAME_WIDTH)
             h = self._capture.get(cv.CAP_PROP_FRAME_HEIGHT)
@@ -31,10 +35,14 @@ class Camera:
     def iterate(self, max_count=None):
         count = 0
         while max_count is None or count < max_count:
-            img = self.capture()
-            if img is not None:
-                yield img
-                count += 1
+            res = self._capture.grab()
+            t = time.time()
+            if res is True:
+                if self._fps is None or self._last is None or t - self._last >= 1/self._fps:
+                    res, img = self._capture.retrieve()
+                    self._last = t
+                    yield img
+                    count += 1
             else:
                 raise StopIteration
 
