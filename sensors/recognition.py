88632017c6a0847129b130cfc1502b39f1d6d341
plume <fortfrederic@free.fr>
2018-02-09 11:39:37 +0100

    sensors/recognition.py:  - added support for bw images  - Improvements on ContourClassifier (still unstable)  - Introduction of SlidingClassifier (needs tweaking)  - Introduction of SplittingClassifier (should work)  - atomic commits are great and why am I doing this ?

--- sensors/recognition.py
+++ sensors/recognition.py
@@ -7,8 +7,8 @@ import pyzbar.pyzbar as zbar
 class Classifier:
     def handle(self, img, draw=False):
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-        return img, list(self._handle(gray, img, draw))
+        thresh, bw = cv.threshold(gray, 100, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+        return img, list(self._handle(gray, img, bw, draw))
 
     def map(self, it, draw=False):
         for img in it:
@@ -36,7 +36,7 @@ class HomoClassifier(Classifier):
         self._search_params = dict(checks = 50)
         self._flann = cv.FlannBasedMatcher(self._index_params, self._search_params)
 
-    def _handle(self, gray, img, draw):
+    def _handle(self, gray, img, bw, draw):
         kp, des = self._sift.detectAndCompute(gray, None)
         if len(kp) > 2 and len(self._kp) > 2:
             matches = self._flann.knnMatch(self._des, des, k=2)
@@ -83,40 +83,124 @@ def children(i, hierarchy):
         yield curr
         curr = hierarchy[curr][0]
 
-def is_mark(i, hierarchy):
+def is_mark(i, hierarchy, contours):
     curr = i
     depth = 0
     while (hierarchy[curr][2] != -1):
+        if cv.isContourConvex(contours[i]):
+            return False
         curr = hierarchy[curr][2]
         depth += 1
-    if (hierarchy[curr][2] != -1):
-        depth += 1
-    return depth == 5
+    return depth >= 5
 
 class ContourClassifier(Classifier):
     def __init__(self):
         pass
 
-    def _handle(self, gray, img, draw):
-        edges = cv.Canny(gray, 100, 200)
+    def _handle(self, gray, img, bw, draw):
+        edges = cv.Canny(bw, 100, 200)
         cntimg, cnt, hierarchy = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
         if hierarchy is not None:
             hierarchy = hierarchy[0]
-            #print(cnt)
             marks = []
             for i,h in enumerate(cnt):
             #h = [next, previous, child, parent]
-                for c in children(i, hierarchy):
-                      if is_mark(c, hierarchy):
-                        marks.append(i)
+                if is_mark(i, hierarchy, cnt):
+                    marks.append(i)
             if len(marks) > 0:
-                print(marks)
-            if draw:
-                for m in marks:
-                    cv.drawContours(img, cnt, m, (0,255,0), 3)
-        
-        yield None
+                if draw:
+                    borders = []
+                    for m in marks:
+                        epsilon = 0.1*cv.arcLength(cnt[m], True)
+                        approx = cv.approxPolyDP(cnt[m], epsilon, True)
+                        cv.drawContours(img, [approx], 0, (0,255,0), 3)
+                        x,y,w,h = cv.boundingRect(approx)
+                        borders.append(((x,y), (w,h)))
+                    x = min([x for (x,y), (w,h) in borders])
+                    y = min([y for (x,y), (w,h) in borders])
+                    w = max([x+w for (x,y), (w,h) in borders]) - x
+                    h = max([y+h for (x,y), (w,h) in borders]) - y
+                    l = max(w,h)
+                    cv.rectangle(img, (x,y), (x+l,y+l), (255,0,0), 2)
+
+                    roi = bw[y:y+h+1,x:x+w+1]
+                    decoded = zbar.decode(roi, symbols=[zbar.ZBarSymbol.QRCODE])
+
+                    yield (x,y,w,h), (int(x+w/2), int(y+h/2)), decoded
+
+class SlidingClassifier(Classifier):
+    def _handle(self, gray, img, bw, draw):
+        decoded = zbar.decode(bw, symbols=[zbar.ZBarSymbol.QRCODE])
+        for code in decoded:
+            x,y = 0,0
+            h,w = bw.shape
+            H,W = bw.shape
+            found = True
+            while found:
+                found = False
+                margin_x = W-w
+                margin_y = H-h
+                cur_x, cur_y = x,y
+                while cur_x + w <= W:
+                    while cur_y + h <= H:
+                        roi = bw[cur_y:cur_y+h+1, cur_x:cur_x+w+1]
+                        test = zbar.decode(roi, symbols=[zbar.ZBarSymbol.QRCODE])
+                        if code in test:
+                            x = max(x,cur_x)
+                            y = max(y,cur_y)
+                            W = min(W,cur_x+w)
+                            H = min(H,cur_y+h)
+                            found = True
+                        cur_x += 2
+                        cur_y += 2
+                w -= 2
+                h -= 2
+            yield (x,y,w,h), (int(x+w/2), int(y+h/2)), code
+
+class SplittingClassifier(Classifier):
+    def _handle(self, gray, img, bw, draw):
+        decoded = zbar.decode(bw, symbols=[zbar.ZBarSymbol.QRCODE])
+        h,w = bw.shape
+        left = bw[:,:int(w/2)]
+        right = bw[:,int(w/2):]
+        top = bw[:int(h/2),:]
+        bottom = bw[int(h/2):,:]
+        ltest = zbar.decode(left, symbols=[zbar.ZBarSymbol.QRCODE])
+        rtest = zbar.decode(right, symbols=[zbar.ZBarSymbol.QRCODE])
+        ttest = zbar.decode(top, symbols=[zbar.ZBarSymbol.QRCODE])
+        btest = zbar.decode(bottom, symbols=[zbar.ZBarSymbol.QRCODE])
+        #if draw:
+            #cv.rectangle(img, (0,0), (int(w/2),h), (255,0,0), 2)
+            #cv.rectangle(img, (int(w/2), 0), (w,h), (0,255,0), 2)
+        for code in decoded:
+            if code in ltest and not code in rtest:
+                lr = -1
+            elif code not in ltest and code in rtest:
+                lr = 1
+            else:
+                lr = 0
+            if code in ttest and not code in btest:
+                tb = 1
+            elif code not in ttest and code in btest:
+                tb = -1
+            else:
+                tb = 0
+            yield lr,tb,code
+            #if code in ltest and code not in rtest:
+                #yield (0,0,int(w/2),h), (int(w/4),int(h/2)), code
+            #elif code not in ltest and code in rtest:
+                #yield (int(w/2), 0, int(w/2),h), (int(3*w/4), int(h/2)), code
+            #else:
+                #yield (0,0,w,h), (int(w/2), int(h/2)), code
+
+class NoClassifier(Classifier):
+    def _handle(self, gray, img, bw, draw):
+        w,h = bw.shape
+        if draw:
+            cv.rectangle(img, (0,0), (h,w), (255,0,0), 2)
+            decoded = zbar.decode(bw, symbols=[zbar.ZBarSymbol.QRCODE])
+        yield (0,0,h,w), (w/2,h/2), decoded
 
 if __name__ == '__main__':
     from sys import argv, exit
@@ -134,22 +218,32 @@ if __name__ == '__main__':
     classifiers.add_argument('--body', action='store_const', const='body', dest='classifier')
     classifiers.add_argument('--homo', action='store_const', const='homo', dest='classifier')
     classifiers.add_argument('--contour', action='store_const', const='contour', dest='classifier')
+    classifiers.add_argument('--none', action='store_const', const='none', dest='classifier')
+    classifiers.add_argument('--slide', action='store_const', const='slide', dest='classifier')
+    classifiers.add_argument('--split', action='store_const', const='split', dest='classifier')
 
     parser.add_argument('--resolution', type=float)
+    parser.add_argument('--fps', type=int)
 
     args = parser.parse_args()
 
-    cam = Camera(args.video, resolution=args.resolution, fps=10)
+    cam = Camera(args.video, resolution=args.resolution, fps=args.fps)
     if args.classifier == 'body':
         cl = BodyClassifier()
     elif args.classifier == 'homo':
-        ref = cv.imread('ref.png', cv.IMREAD_UNCHANGED)
+        ref = cv.imread('ref2.png', cv.IMREAD_UNCHANGED)
         cl = HomoClassifier(ref)
         cv.imshow('ref', ref)
         cv.waitKey(1000)
         cv.destroyAllWindows()
     elif args.classifier == 'contour':
         cl = ContourClassifier()
+    elif args.classifier == 'none':
+        cl = NoClassifier()
+    elif args.classifier == 'slide':
+        cl = SlidingClassifier()
+    elif args.classifier == 'split':
+        cl = SplittingClassifier()
     else:
         exit(-2)
 
